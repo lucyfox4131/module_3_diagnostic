af9d706ee1d8400d521de1f40662466ecdc397c1
Lucy Fox <lucyreneefox@gmail.com>
2016-07-25 10:33:36 -0600

    Adding params to search

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -1,11 +1,22 @@
 class NrelService
 
-  def initialize
+  def initialize(api_key)
     @_connection = Faraday.new("https://developer.nrel.gov")
+    @_api_key = api_key
   end
 
   def stations_by_zip(zip_code)
-    connection.get("/api/alt-fuel-stations/v1/nearest.json")
+    response = connection.get("/api/alt-fuel-stations/v1/nearest.json"), {:api_key => ENV["NRELAPIKEY"],
+                                                                          :format => JSON,
+                                                                          :zip => zip_code,
+                                                                          :raidus => "6.0",
+                                                                          :fuel_type => ["ELEC", "LPG"],
+                                                                          :limit => 10}
+    parse(response)
+  end
+
+  def parse(response)
+    JSON.parse(response.body)
   end
 
   private
