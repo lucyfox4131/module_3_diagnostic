f9cf304c4932bb905ae4541f268300d58a7ab00c
Lucy Fox <lucyreneefox@gmail.com>
2016-07-25 11:14:39 -0600

    view formatting for display of information by station.

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,18 +1,27 @@
 class Station < OpenStruct
 
-  def initialize
+  def get_connection
     @_nrel_conn ||= NrelService.new
   end
 
   def get_stations(zip_code)
+    get_connection
     stations = nrel_conn.stations_by_zip(zip_code)
     create_stations(stations['fuel_stations'])
   end
 
   def create_stations(stations)
-    stations.each do |station|
+    stations.map do |station|
       Station.new(station)
     end
+  end
+
+  def address
+    "#{self.street_address}, #{self.city}, #{self.state}, #{self.zip}"
+  end
+
+  def fuel_types
+    self.fuel_type_code
   end
 
   private
